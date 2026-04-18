dc79a660fba909b9bf98baf65d1fd25094cbcae5
SaroshGabriel <saroshjibreel@gmail.com>
2026-04-18 12:26:34 +0530

    fix: clean hyprland.conf, fade wallpaper transitions, add nifty launcher, fix hypridle dpms

---
+++ configs/hypr/scripts/launch-nifty.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ~/.bashrc
+cd ~/LinuxScripts/niftyMonitor
+source .venv/bin/activate
+python stock_monitor.py
--- configs/hypr/wallpaper.sh
+++ configs/hypr/wallpaper.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
-
 mapfile -t walls < <(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \))
 
 # Ensure daemon is running
@@ -8,13 +7,14 @@ if ! pgrep -x awww-daemon > /dev/null; then
     awww-daemon &>/dev/null &
     sleep 3
 fi
+
 sleep 2
 
 while true; do
     wall1="${walls[$RANDOM % ${#walls[@]}]}"
     wall2="${walls[$RANDOM % ${#walls[@]}]}"
-    awww img --outputs HDMI-A-2 --transition-type random "$wall1" &>/dev/null
-    awww img --outputs eDP-1 --transition-type random "$wall2" &>/dev/null
+    awww img --outputs HDMI-A-2 --transition-type fade --transition-duration 2 "$wall1" &>/dev/null
+    awww img --outputs eDP-1 --transition-type fade --transition-duration 2 "$wall2" &>/dev/null
     sleep 300
     if ! pgrep -x awww-daemon > /dev/null; then
         awww-daemon &>/dev/null &
